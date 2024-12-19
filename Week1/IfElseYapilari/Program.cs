5231fa5ecf2177b3c68d501f4c169bc3acc1c437
Halenur İncedere <halenurincedere@gmail.com>
2024-12-19 12:57:59 +0300

    Added If/Else example for Week1

---
+++ Week1/IfElseYapilari/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Take input from the user
+        Console.Write("Please enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the number is smaller than, equal to, or greater than 10
+        if (number < 10)
+        {
+            Console.WriteLine("The number you entered is smaller than 10.");
+        }
+        else if (number == 10)
+        {
+            Console.WriteLine("The number you entered is equal to 10.");
+        }
+        else
+        {
+            Console.WriteLine("The number you entered is greater than 10.");
+        }
+
+        // Check if the number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number you entered is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number you entered is odd.");
+        }
+    }
+}
---
+++ Week1/IfElseYapilari/README.md
@@ -0,0 +1,14 @@
+# If / Else Practice
+
+This is a basic console application created as part of the **PatikaDev Bootcamp**.
+
+## 📜 Task Description
+
+This practice involves:
+1. Accepting a number input from the user.
+2. Checking if the entered number is equal to 10, less than 10, or greater than 10, and displaying an informative message to the user.
+3. Determining whether the entered number is even or odd and printing the result.
+
+### Example Console Output:
+
+Please enter a number: 22 The entered number is greater than 10. The entered number is even.
